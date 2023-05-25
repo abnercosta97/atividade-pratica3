208d3cd0f9749bd9ae1b49d6c91eeecab48cacce
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-05-24 21:52:22 -0300

    exercicio 9 incompleto

---
+++ src/exercicio9.js
@@ -0,0 +1,15 @@
+function fatorial(nro) {
+  let total = 1;
+  for (let i = 1; i <= nro; i++) {
+    _______________;
+  }
+  return total;
+}
+let r = fatorial(5);
+console.log("Fatorial:", r);
+r = fatorial(2);
+console.log("Fatorial:", r);
+r = fatorial(1);
+console.log("Fatorial:", r);
+r = fatorial(0);
+console.log("Fatorial:", r);
